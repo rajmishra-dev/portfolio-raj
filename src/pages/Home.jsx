fefaef68bbbcaea81a11aa5220dd928ed77e6d8f
Raj Mishra <shradkumar002@gmail.com>
2026-06-04 21:10:01 +0530

    Update protfolio project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export const Home = () => {
     return (
-        <div className="min-h-screen container ">
+        <div className="min-h-screen bg-background text-forground overflow-x-hidden">
             {/* Theme Toggle */}
-
+             <ThemeToggle/>
             {/* Background Effect */}
 
             {/* Navebar */}
