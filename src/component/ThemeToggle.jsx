a1a93d4a6d0661c82627c60eeeb0c18296ec8e74
Raj Mishra <shradkumar002@gmail.com>
2026-06-05 23:03:36 +0530

    Add dark mode theme toggle

--- src/component/ThemeToggle.jsx
+++ src/component/ThemeToggle.jsx
@@ -4,14 +4,25 @@ import { useState } from "react";
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if (storedTheme === "dark") {
+            setIsDarkMode(true);
+            document.documentElement.classList.remove("dark");
+        }
+    }, []);
+
     const toggleTheme = () => {
         if (isDarkMode) {
             document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
             setIsDarkMode(false);
-    }else{
+        } else {
             document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
             setIsDarkMode(true);
-    }
-    return  <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" />: <Moon className="h-6 w-6 text-blue-900" />}</button>
-    
+        }
+    };
+
+    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>;
 }
