13c17f4c4f5c75223631fda72411ea256bd76834
Raj Mishra <shradkumar002@gmail.com>
2026-06-05 23:56:40 +0530

    I add some new lib.js file in project

--- src/component/ThemeToggle.jsx
+++ src/component/ThemeToggle.jsx
@@ -9,7 +9,7 @@ export const ThemeToggle = () => {
         if (storedTheme === "dark") {
             setIsDarkMode(true);
             document.documentElement.classList.add("dark");
-        }else{
+        } else {
             setIsDarkMode(false);
             document.documentElement.classList.remover("light");
         }
@@ -27,5 +27,6 @@ export const ThemeToggle = () => {
         }
     };
 
-    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>;
+    return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z50 p-2 rounded-full transition-colors duration-300", "focus:outline-hidden")}>
+        {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>;
 }
