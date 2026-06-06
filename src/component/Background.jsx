8a65a4b77c861ddcb393619b6ce98c1d85251977
Raj Mishra <shradkumar002@gmail.com>
2026-06-06 10:03:11 +0530

    In Background file I added some new componenet

--- src/component/Background.jsx
+++ src/component/Background.jsx
@@ -1,4 +1,4 @@
-import { useState}from "react";
+import { useState, useEffect } from "react";
 export const BackgroundAnimations = () => {
 
     //id,size,x,y, opacity, animatonDuration
@@ -17,15 +17,28 @@ export const BackgroundAnimations = () => {
                 id: i,
                 size: Math.random() * 3 + 1,
                 x: Math.random() * 100,
-                y: Math.random() * 100;
+                y: Math.random() * 100,
                 opacity: Math.random() * 0.4 + 0.3,
                 animationDuration: Math.random() * 5 + 5,
                 color: color,
-
-        })  
-      }
+            });
+        }
+        setStars(newStars);
     };  
     return (
-        <div></div>
+        <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+            {stars.map((item) => (
+                <div key={item.id} className="absolute rounded-full animation-float" style={{
+                    width: `${item.size}px`,
+                    height: `${item.size}px`,
+                    left: `${item.x}%`,
+                    top: `${item.y}%`,
+                    backgroundColor: `${item.color}`,
+                    opacity: `${item.opacity}`,
+                    animationDuration : `${item.animationDuration}`,
+                    animationDelay: `${Math.random() * 2}s`
+                }} />
+            ))}
+        </div>
     );
 }
