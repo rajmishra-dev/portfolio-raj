97563758c73c6b4452b919fd04cb2fd82b02a21b
Raj Mishra <shradkumar002@gmail.com>
2026-06-18 22:43:15 +0530

    UPdate a portfolio

---
+++ backend-portfolio/src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
+import Skills from "../pages/Skills";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
